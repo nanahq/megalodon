14aabe817fe931e30f28af2f1b6425e16a7f49c8
siradji <siradjiawoual@gmail.com>
2024-04-16 02:44:57 +0100

    feature: new onboarding dashboard

---
+++ mobile-app/screens/OnboardingNavigator/screens/components/PaginationComponent.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {  StyleSheet } from "react-native";
+import { Pagination, PaginationProps } from "react-native-swiper-flatlist";
+
+export function CarouselPagination(props: PaginationProps): JSX.Element {
+    return <Pagination {...props} paginationStyle={styles.paginationContainer} />;
+}
+
+
+
+const styles = StyleSheet.create({
+    paginationContainer: {
+        bottom: 0,
+        height: 6,
+        marginBottom: 64,
+        marginVertical: 0,
+        position: undefined,
+    },
+});
