39942517076755681da34a44ec1bc25a92aaf11e
siradji <siradjiawoual@gmail.com>
2024-04-30 07:21:04 +0100

    feature: onboarding screen

--- mobile-app/screens/OnboardingNavigator/screens/Onboarding.screen.tsx
+++ mobile-app/screens/OnboardingNavigator/screens/Onboarding.screen.tsx
@@ -1,16 +1,18 @@
-import {View, Text, Image, Dimensions} from 'react-native'
+import {View, Text, Image, Dimensions, ImageBackground} from 'react-native'
 import {tailwind} from "@tailwind";
 import * as SplashScreen from 'expo-splash-screen'
 import {useEffect} from 'react'
 import {useLogger} from "@contexts/NativeLoggingProvider";
-import {GenericButton} from "@components/commons/buttons/GenericButton";
+import {GenericButton, GenericButtonLink} from "@components/commons/buttons/GenericButton";
 import {NavigationProp, useNavigation} from "@react-navigation/native";
 import {OnboardingParamsList} from "@screens/OnboardingNavigator/OnboardingNav";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {OnboardingCarousel} from "@screens/OnboardingNavigator/screens/components/OnboardingCarosel";
 import OnboardingCover from '@assets/onboarding/COVER_2.png'
 import {OnboardingScreenName} from "@screens/OnboardingNavigator/ScreenName.enum";
-
+import {LinearGradient} from "expo-linear-gradient";
+import OnboardingCoverImage from '@assets/onboarding/onboarding_image_4.png'
+import NanaIcon from '@assets/onboarding/nana_logo_White.png'
 export function OnboardingScreen (): JSX.Element {
     const navigator = useNavigation<NavigationProp<OnboardingParamsList>>()
     const logger = useLogger()
@@ -24,29 +26,41 @@ export function OnboardingScreen (): JSX.Element {
     const {width, height} = Dimensions.get('window')
     return (
         <View
-            style={tailwind("flex-1")}
-            testID="onboarding_carousel"
+            style={tailwind('flex flex-col w-full flex-1 bg-white')}
+            testID="OnboardingScreen.View"
         >
-            <View style={tailwind("flex-1")}>
-               <Image
-                resizeMode="cover"
-                style={{width, height}}
-                source={OnboardingCover}
-               />
-            </View>
-            <View style={tailwind('px-8 flex flex-col')}>
-                <View style={tailwind("flex flex-col items-center justify-center w-full")}>
-                    <Text style={tailwind('text-black mb-3 font-bold text-3xl')}>Welcome to Nana</Text>
-                    <Text style={tailwind('text-center text-gray-600 text-lg')}>Nana = Delivered! Enjoy the best food or what you need nearby, delivered.</Text>
+            <ImageBackground
+                source={OnboardingCoverImage}
+                contentFit="cover"
+                style={tailwind('flex-1')}
+            >
+                <LinearGradient
+                    colors={['rgba(0,0,0,0.8)', 'rgba(0,0,0,0.5)']} // Adjust the overlay color and opacity here
+                    style={tailwind('absolute top-0 left-0 right-0 bottom-0')}
+                />
+                <View style={tailwind('flex-1 justify-end w-full px-3')}>
+                    <View style={tailwind('mb-5')}>
+                      <View style={{width: 150, height:75}}>
+                          <Image
+                              source={NanaIcon}
+                              resizeMode="contain"
+                              style={{width: 150, height:75}}
+                          />
+                      </View>
+                        <Text style={tailwind('mt-4 text-gray-100 text-3xl text-gray-200')}>Super fast Food and Groceries delivery</Text>
+                    </View>
+                    <View style={tailwind('w-full bg-gray-100 h-0.5')} />
+                    <View style={tailwind('flex flex-col mt-14 justify-center items-center mb-10')}>
+                        <GenericButton
+                            style={tailwind('mb-1')}
+                            labelColor={tailwind('text-white')}
+                            onPress={() => navigator.navigate<any>(OnboardingScreenName.ENTER_MOBILE_PHONE)}
+                            label="Get Started"
+                            backgroundColor={tailwind('bg-primary-100 w-full')}
+                        />
+                    </View>
                 </View>
-               <GenericButton
-                   style={tailwind('mt-14 mb-8')}
-                   labelColor={tailwind('text-white')}
-                   onPress={() => navigator.navigate(OnboardingScreenName.ENTER_MOBILE_PHONE)}
-                   label="Get Started"
-                   backgroundColor={tailwind('bg-primary-100')}
-               />
-            </View>
+            </ImageBackground>
         </View>
     )
 }
