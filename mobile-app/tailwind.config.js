0c77455e0ce27c47e5a83d9eb96c8fb24919ac48
Suraj Auwal <siradjiawoual@gmail.com>
2024-10-17 10:52:19 +0100

    One signal installation

--- mobile-app/tailwind.config.js
+++ mobile-app/tailwind.config.js
@@ -3,25 +3,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: {
-          50: "#F7FBFE",
-          100: "#EFF6FD",
-          200: "#D6EAF9",
-          300: "#BDDDF5",
-          500: "#5AA9E6",
-          600: "#5198CF",
-          700: "#447FAD",
-          900: "#2C5371",
-        },
-        darkblue: {
-          50: "#191D21",
-          100: "#3E627A",
-          200: "#4C7895",
-          300: "#5F96BA",
-          500: "#7FC8F8",
-          600: "#A5D9FA",
-          700: "#CCE9FC",
-          900: "#F2FAFE",
+        nana: {
+          primary: "#469ADC",
+          secondary: "#2C3E50",
+          red: "#DC3545",
+          green: "#28A745"
         },
         primary: {
           50: "#ecf7fd",
@@ -35,69 +21,12 @@ module.exports = {
           800: '#32803e',
           900: '#16371b',
         },
-        secondary: {
-          100: '#feefe6',
-          200: '#fdceb4',
-          300: '#fcae83',
-          400: '#fcae83',
-          500: '#Fa803d',
-          600: '#f96d1f',
-          700: '#f96d1f',
-          800: '#ae4104',
-          900: '#ae4104',
-        },
-
-        accent: {
-          500: '#ffffff',
-        },
-
-        'brand-blue': {
-          200: '#DBE1E5',
-          500: '#0b1c3a',
-        },
-        'nana-text': "#4c5270",
-        'brand-black': {
-          500: '#4c5270',
-        },
-        'brand-gray': {
-          200: '#E6E6E6',
-          500: '#F6F6F9',
-          400: '#8D959B',
-          700: '#B5B5B5',
-          800: '#717171'
-        },
-        'brand-gray-text': {
-          500: '#0b1c3a99',
-        },
-        'brand-green': {
-          500: '#4FD3BC'
-        },
-        'nana-lime': '#4ADE80',
-        success: {
-          50: "#F2FBF4",
-          100: "#E6F7E8",
-          200: "#6FFD83",
-          300: "#9AE1A4",
-          500: "#02B31B",
-          600: "#12BB41",
-        },
-        error: {
-          50: "#FFE5E5",
-          100: "#FFE6E6",
-          200: "#FF9999",
-          300: "#FF6666",
-          500: "#FF0000",
-          600: "#E60000",
-        },
-        warning: {
-          50: "#FFFAF4",
-          100: "#FFF5EA",
-          200: "#FFD2A3",
-          300: "#FFD5A9",
-          500: "#FF9629",
-          600: "#E68725",
-        },
         'brand-ash': '#EEEEEE'
+      },
+      fontsize: {
+        'nana-text': 10,
+        'nana-subheading': 20,
+        'nana-heading': 30
       },
     borderWidth: {
       0.5: 0.5,
